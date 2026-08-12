90a3c46320df8f3d1d1ebdeea8c029783e6937ea
Dakin <nthanhnam648@gmail.com>
2026-08-12 16:21:58 +0700

    Code Menu

---
+++ Assets/Script/UI/Menu/MainMenuActions.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.UI.Menu
+{
+    /// <summary>
+    /// Chịu trách nhiệm duy nhất: bắt đầu game và thoát game.
+    /// </summary>
+    public class MainMenuActions : MonoBehaviour
+    {
+        [SerializeField] private string gameSceneName = "Game";
+
+        /// <summary>
+        /// Được gọi từ StartButton.OnClick.
+        /// </summary>
+        public void StartShift()
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
+
+        /// <summary>
+        /// Được gọi từ QuitButton.OnClick.
+        /// Lưu ý: Application.Quit() không thoát Play Mode trong Unity Editor,
+        /// cần test hành vi thực tế trong Build.
+        /// </summary>
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Script/UI/Menu/MenuPanelController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Game.UI.Menu
+{
+    /// <summary>
+    /// Chịu trách nhiệm duy nhất: quản lý hiển thị MainPanel, SettingsPanel, CreditsPanel.
+    /// Tại mọi thời điểm chỉ có một panel active.
+    /// </summary>
+    public class MenuPanelController : MonoBehaviour
+    {
+        [SerializeField] private GameObject mainPanel;
+        [SerializeField] private GameObject settingsPanel;
+        [SerializeField] private GameObject creditsPanel;
+
+        /// <summary>
+        /// Được gọi từ Settings BackButton / Credits BackButton.OnClick.
+        /// </summary>
+        public void ShowMainPanel()
+        {
+            ShowOnly(mainPanel);
+        }
+
+        /// <summary>
+        /// Được gọi từ SettingsButton.OnClick.
+        /// </summary>
+        public void ShowSettingsPanel()
+        {
+            ShowOnly(settingsPanel);
+        }
+
+        /// <summary>
+        /// Được gọi từ CreditsButton.OnClick.
+        /// </summary>
+        public void ShowCreditsPanel()
+        {
+            ShowOnly(creditsPanel);
+        }
+
+        private void ShowOnly(GameObject selectedPanel)
+        {
+            mainPanel.SetActive(false);
+            settingsPanel.SetActive(false);
+            creditsPanel.SetActive(false);
+
+            selectedPanel.SetActive(true);
+        }
+    }
+}
